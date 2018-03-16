b3796c6053d035da2eeed24670d689b651dc41b9
pczhang <zhangpc123@outlook.com>
2018-03-16 21:01:21 +0800

    update documents

--- gen-site.sh
+++ gen-site.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdocs build &&
-cd site
-git add -A
-git commit -m "update site"
+git add -A && git commit -m "update documents"
+
+mkdocs build && \
+cd site && \
+git add -A && \
+git commit -m "update site" && \
 git push
